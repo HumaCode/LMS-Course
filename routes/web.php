ee56c11e359d4006bac9097d645e77eb104963e5
HumaCode <humaidizakaria1998@gmail.com>
2024-01-04 16:00:48 +0700

    user dashboard setup part 1 - user dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/', [UserController::class, 'index'])->name('index');
 
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('frontend.dashboard.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
