34c1dfea8e7bd0df304b8d8c9be9095d16c030c8
HumaCode <humaidizakaria1998@gmail.com>
2024-01-19 21:39:24 +0700

    course display part 1 - frontend home

--- app/Providers/ViewComposerServiceProvider.php
+++ app/Providers/ViewComposerServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Category;
+use App\Models\Course;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,6 +27,17 @@ class ViewComposerServiceProvider extends ServiceProvider
 
 
             $view->with(['categories' => $categories]);
+        });
+
+        View::composer('frontend.home.course-area', function ($view) {
+            $courses = Course::where('status', 1)->orderBy('id', 'ASC')->limit(6)->get(); // Misalnya, Anda ingin mengambil data user saat ini
+            $categories = Category::orderBy('category_name', 'ASC')->get(); // Misalnya, Anda ingin mengambil data user saat ini
+
+
+            $view->with([
+                'courses' => $courses,
+                'categories' => $categories,
+            ]);
         });
     }
 }
