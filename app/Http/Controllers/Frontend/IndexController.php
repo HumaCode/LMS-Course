a1c2eb6c28c8b43f673e6c1029b32687cb531182
HumaCode <humaidizakaria1998@gmail.com>
2024-01-20 22:40:50 +0700

    frontend setup sourse details page part 4

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Course;
 use App\Models\Course_goal;
+use App\Models\CourseLecture;
+use App\Models\CourseSection;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function courseDetail($id, $slug)
     {
-        $course = Course::with('category', 'subcategory', 'user')->find($id);
-        $goals = Course_goal::where('course_id', $course->id)->orderBy('id', 'DESC')->get();
+        $course     = Course::with('category', 'subcategory', 'user')->find($id);
+        $goals      = Course_goal::where('course_id', $course->id)->orderBy('id', 'DESC')->get();
+        $lecture    = CourseLecture::where('course_id', $course->id)->get();
+        $sections   = CourseSection::where('course_id', $course->id)->orderBy('id', 'ASC')->get();
 
-        return view('frontend.course.course_details', compact('course', 'goals'));
+        return view('frontend.course.course_details', compact('course', 'goals', 'lecture', 'sections'));
     }
 }
