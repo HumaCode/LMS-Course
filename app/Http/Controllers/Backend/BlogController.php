73396b0c1120ee56b2aade63962e625bfb88fddf
HumaCode <humaidizakaria1998@gmail.com>
2024-03-17 16:23:31 +0700

    custom pagination part 1 - frontend

--- app/Http/Controllers/Backend/BlogController.php
+++ app/Http/Controllers/Backend/BlogController.php
@@ -289,7 +289,7 @@ class BlogController extends Controller
     ////////////////////////////////////////////
     public function blogList()
     {
-        $blog       = BlogPost::latest()->get();
+        $blog       = BlogPost::latest()->paginate(2);
         $bcategory  = BlogCategory::latest()->get();
         $post       = BlogPost::latest()->limit(3)->get();
 
